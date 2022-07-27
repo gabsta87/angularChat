3596e2f8965990e0cbbd7be4bea021022c482708
gabsta87 <gabrielmaretsecondaire@gmail.com>
2022-07-27 17:43:11 +0200

    corrected imports after removing useless files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
-import { MessageslistComponent } from './features/ui/messageslist/messageslist.component';
-import { EntryboxComponent } from './features/ui/entrybox/entrybox.component';
-import { ChatslistComponent } from './features/ui/chatslist/chatslist.component';
-import { EventdetailsComponent } from './features/ui/eventdetails/eventdetails.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessageslistComponent,
-    EntryboxComponent,
-    ChatslistComponent,
-    EventdetailsComponent
   ],
   imports: [
     BrowserModule,
