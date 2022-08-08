9d2c3d62cddc297a617e51b0ec83bead5d934791
gabsta87 <gabrielmaretsecondaire@gmail.com>
2022-08-08 15:24:37 +0200

    interface added but not used

---
+++ src/app/shared/service/dataAccess.ts
@@ -0,0 +1,40 @@
+import { User } from "@angular/fire/auth";
+
+export interface DataAccess{
+
+    // requests
+    getPendingRequests():any;
+
+    createPendingRequest(name:string, userId:string):any;
+
+    deletePendingRequest(requestId:string):any;
+
+    // activities
+    getActivities():any;
+
+    getActivity(activityId:string):any;
+
+    createActivity(name:string):any;
+
+    // events
+    getEvents():any;
+
+    createEvent(name:string,creatorId:string,date:string,location:string):any;
+
+    deleteEvent(eventId:string):any;
+
+    // getEventsFiltered(location:string,distance:number,...types:string[]):any;
+    // getEventsFiltered(name:string,location:string,distance:number,...types:string[]):any;
+
+    // users
+    getUsers():any;
+
+    getUser(userId:string):any;
+
+    createUser(newUser:User):any;
+
+    // messages
+    getMessages(discussionId:string,count?:number):any;
+
+    writeMessage(discussionId:string,message:string,type:string,user:User|null):any;
+}
