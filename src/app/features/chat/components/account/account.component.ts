3cce1a77b773ecbba03395d40f7aff4cf6e67de1
gabsta87 <gabrielmaretsecondaire@gmail.com>
2022-08-19 10:45:01 +0200

    events lists added in account

--- src/app/features/chat/components/account/account.component.ts
+++ src/app/features/chat/components/account/account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth, GoogleAuthProvider, signInAnonymously, signOut } from '@angular/fire/auth';
 import { signInWithPopup } from '@firebase/auth';
+import { firstValueFrom } from 'rxjs';
 import { AngularfireService } from 'src/app/shared/service/angularfire.service';
 
 @Component({
@@ -8,11 +9,34 @@ import { AngularfireService } from 'src/app/shared/service/angularfire.service';
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
-export class AccountComponent{
+export class AccountComponent implements OnInit{
 
   autoLocation!:boolean;
+  attendedEvents!:any;
+  createdEvents!:any;
 
-  constructor(private readonly _auth: Auth,private readonly _dbAccess:AngularfireService) { }
+  constructor(private readonly _auth: Auth,private readonly _dbAccess:AngularfireService) {
+  }
+  
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  onViewWillEnter(){
+  }
+
+  async loadData(){
+    console.log("loading data");
+    
+    let userId = this._auth?.currentUser?.uid;
+    if(userId){
+      this.attendedEvents = await firstValueFrom(this._dbAccess.getEventsAttendedBy(userId));
+      console.log("attended events : ",this.attendedEvents);
+      
+      this.createdEvents = await firstValueFrom(this._dbAccess.getEventsCreatedBy(userId));
+      console.log("created events : ",this.createdEvents);
+    }
+  }
 
   async logout(){
     await signOut(this._auth);
@@ -27,6 +51,7 @@ export class AccountComponent{
     const credential = await signInWithPopup(this._auth,provider);
     this._dbAccess.createUser(credential.user);
     console.log("credential = ",credential);
+    this.loadData();
   }
 
   async loginAnonymously(){
