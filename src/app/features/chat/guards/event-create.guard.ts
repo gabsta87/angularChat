9cb6ab9c48610ce532976d15edc3b89829cc0211
gabsta87 <gabrielmaretsecondaire@gmail.com>
2022-08-24 11:49:17 +0200

    event creation guard corrected

--- src/app/features/chat/guards/event-create.guard.ts
+++ src/app/features/chat/guards/event-create.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,13 +8,13 @@ import { Observable } from 'rxjs';
 })
 export class EventCreateGuard implements CanActivate {
 
-  constructor(private readonly _auth:Auth){}
+  constructor(private readonly _auth:Auth,private readonly _router: Router){}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    console.log("route : ",route,", state : ",state);
-    
+    if(this._auth.currentUser?.uid === undefined)
+      this._router.navigate(["pendingevents"])
     return this._auth.currentUser?.uid !== undefined;
   }
   
